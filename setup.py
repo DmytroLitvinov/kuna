ce8847a9e2e32106e498e2241e3a1a627af4fd34
Dmytro Litvinov <litvinov.dmytro.it@gmail.com>
2017-10-29 10:19:58 +0200

    Update version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='kuna',
-    version='0.1.0',
+    version='0.1.1',
     description="Interact with Kuna exchange API",
     long_description=readme + '\n\n' + history,
     author="Dmytro Litvinov",
