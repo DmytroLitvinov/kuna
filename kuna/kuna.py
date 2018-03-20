8bbf15e2a1f34264ef3abaab667359ce2a5494ad
Dmytro Litvinov <litvinov.dmytro.it@gmail.com>
2018-03-20 23:03:38 +0200

    Changelog for upcoming release 0.2.11.

--- kuna/kuna.py
+++ kuna/kuna.py
@@ -17,7 +17,7 @@ API_VERSION = '2'
 KUNA_API_URL_PREFIX = 'api/v{}'.format(API_VERSION)
 KUNA_API_BASEURL = 'https://kuna.io/{}/'.format(KUNA_API_URL_PREFIX)
 
-VALID_MARKET_DATA_PAIRS = ['btcuah', 'ethuah', 'wavesuah', 'gbguah', 'eosbtc', 'bchuah', 'xrpuah', 'xlmuah', 'tusduah',
+VALID_MARKET_DATA_PAIRS = ['btcuah', 'ethuah', 'wavesuah', 'gbguah', 'eosuah', 'bchuah', 'xrpuah', 'xlmuah', 'tusduah',
                            'golgbg', 'bchbtc', 'kunbtc', 'rmcbtc', 'rbtc', 'arnbtc', 'evrbtc', 'b2bbtc',
                            'foodbtc', 'otxbtc', 'hknbtc']
 
