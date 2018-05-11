4d7496a8746f7ce6bbce8caf584063bdfcddcde1
Dmytro Litvinov <litvinov.dmytro.it@gmail.com>
2018-05-11 13:14:27 +0300

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='kuna',
-    version='0.3.1',
+    version='0.3.2',
     description="Interact with Kuna exchange API",
     long_description=readme + '\n\n' + history,
     author="Dmytro Litvinov",
